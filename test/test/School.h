9db7948be2b1d2586c5163bb58f746b78f8290cd
yuanxinliang <xinliang.yuan@livibank.com>
2023-03-28 18:33:40 +0800

    hh

---
+++ test/test/School.h
@@ -0,0 +1,16 @@
+//
+//  School.h
+//  test
+//
+//  Created by xinliang.yuan on 2023/3/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface School : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test/test/School.m
@@ -0,0 +1,12 @@
+//
+//  School.m
+//  test
+//
+//  Created by xinliang.yuan on 2023/3/28.
+//
+
+#import "School.h"
+
+@implementation School
+
+@end
