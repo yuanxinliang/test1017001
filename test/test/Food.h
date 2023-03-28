be6bff4df5cb9eef344b048b0aac359e56672c28
yuanxinliang <xinliang.yuan@livibank.com>
2023-03-28 18:10:08 +0800

    commit-05

---
+++ test/test/Food.h
@@ -0,0 +1,16 @@
+//
+//  Food.h
+//  test
+//
+//  Created by xinliang.yuan on 2023/3/28.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Food : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ test/test/Food.m
@@ -0,0 +1,12 @@
+//
+//  Food.m
+//  test
+//
+//  Created by xinliang.yuan on 2023/3/28.
+//
+
+#import "Food.h"
+
+@implementation Food
+
+@end
